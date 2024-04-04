abd1f7da9525146e58b91db280c36ec37c1054b1
jacobop2 <jacobop2@illinois.edu>
2024-04-03 19:03:12 -0500

    functional database

--- code/sql-setup/tables.sql
+++ code/sql-setup/tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE Artists (
 );
 
 CREATE TABLE Songs (
-  songid VARCHAR(50),
+  songid VARCHAR(22),
   songname VARCHAR(255),
   len INT,
   popularity INT,
@@ -29,9 +29,7 @@ CREATE TABLE Posts (
   timeofpost VARCHAR(20),
   likes INT,
   userid INT,
-  songid VARCHAR(50),
-  CONSTRAINT FK_userid_P FOREIGN KEY (userid) REFERENCES UserAccounts(userid) ON DELETE CASCADE,
-  CONSTRAINT FK_songid_P FOREIGN KEY (songid) REFERENCES Songs(songid) ON DELETE CASCADE,
+  songid VARCHAR(22),
   PRIMARY KEY (postid)
 );
 
@@ -44,31 +42,23 @@ CREATE TABLE Tags (
 CREATE TABLE Creates (
   postid INT,
   tagid INT,
-  CONSTRAINT FK_postid_C FOREIGN KEY (postid) REFERENCES Posts(postid) ON DELETE CASCADE,
-  CONSTRAINT FK_tagid_C FOREIGN KEY (tagid) REFERENCES Tags(tagid) ON DELETE CASCADE,
   PRIMARY KEY (postid, tagid)
 );
 
 CREATE TABLE ListeningHistories (
   userid INT,
-  songid VARCHAR(50),
-  CONSTRAINT FK_userid_LH FOREIGN KEY (userid) REFERENCES UserAccounts(userid) ON DELETE CASCADE,
-  CONSTRAINT FK_songid_LH FOREIGN KEY (songid) REFERENCES Songs(songid) ON DELETE CASCADE,
+  songid VARCHAR(22),
   PRIMARY KEY (userid, songid)
 );
 
 CREATE TABLE Friends (
   userid1 INT,
   userid2 INT,
-  CONSTRAINT FK_userid1_F FOREIGN KEY (userid1) REFERENCES UserAccounts(userid) ON DELETE CASCADE,
-  CONSTRAINT FK_userid2_F FOREIGN KEY (userid2) REFERENCES UserAccounts(userid) ON DELETE CASCADE,
   PRIMARY KEY (userid1, userid2)
 );
 
 CREATE TABLE HasSongs (
   artistid VARCHAR(50),
-  songid VARCHAR(50),
-  CONSTRAINT FK_artistid_HS FOREIGN KEY (artistid) REFERENCES Artists(artistid) ON DELETE CASCADE,
-  CONSTRAINT FK_songid_HS FOREIGN KEY (songid) REFERENCES Songs(songid) ON DELETE CASCADE,
+  songid VARCHAR(22),
   PRIMARY KEY (artistid, songid)
 );
