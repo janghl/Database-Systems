72474f97cafd0516d819578a54e26b0cf5ff161d
pjj3 <pjj3@illinois.edu>
2024-04-26 00:48:37 -0500

    Made some progress on posts

--- code/app-react-express/client/src/components/Post.tsx
+++ code/app-react-express/client/src/components/Post.tsx
@@ -1,24 +1,37 @@
-import "../App.css"; // Import the CSS file for styling
+import React from "react";
 
+// Import the CSS file for styling
+import "../App.css";
+
+// Define the interface for PostProps
 interface PostProps {
-    SongName: string;
-    UserName: string;
-    Artist: string;
-    TimeOfPost: string;
-    Rating: number;
-    AlbumImageURL: string;
-  }
+  SongName: string;
+  UserName: string;
+  Artist: string;
+  TimeOfPost: string;
+  Rating: number;
+}
 
-function Post({SongName, UserName, Artist, TimeOfPost, Rating, AlbumImageURL}: PostProps) {
+function Post({ SongName, UserName, Artist, TimeOfPost, Rating }: PostProps) {
+  const getImageUrl = () => {
+    const ratingImages: { [key: number]: string } = {
+      0: "https://i.imgur.com/G5i4y9h.png",
+      1: "https://i.imgur.com/3RvL8zq.png",
+      2: "https://i.imgur.com/YPxzNGe.png",
+      3: "https://i.imgur.com/0B5SeCt.png",
+      4: "https://i.imgur.com/YhQtKmk.png",
+      5: "https://i.imgur.com/9V64Cbu.png"
+    };
+
+    return ratingImages[Rating];
+  };
+
   return (
     <div className="post">
       <h2>{UserName}</h2>
-      <h1>{SongName}</h1>
-      <p>{Artist}</p>
+      <h1>{SongName} by {Artist}</h1>
       <p>{TimeOfPost}</p>
-      <div className="post-album-img">
-        <img src = {AlbumImageURL} alt = "Couldn't Retrieve Image" />
-      </div>
+      <img src={getImageUrl()} alt={`Rating ${Rating}`} />
     </div>
   );
 }
--- code/app-react-express/client/src/pages/Posts.tsx
+++ code/app-react-express/client/src/pages/Posts.tsx
@@ -7,7 +7,11 @@ function Posts() {
     <div className="screen">
       <NavBar />
       <div className="postContainer">
-        <Post SongName = "Song name" UserName = "User name" Artist = "Artist" TimeOfPost = "Time" Rating = {5} AlbumImageURL = "https://pbs.twimg.com/media/EW41YTBWoAIcjOk.jpg:large"/>
+        <Post SongName = "Song name" UserName = "User name" Artist = "Artist" TimeOfPost = "Time" Rating = {5}/>
+        <Post SongName = "Song name" UserName = "User name" Artist = "Artist" TimeOfPost = "Time" Rating = {3}/>
+        <Post SongName = "Song name" UserName = "User name" Artist = "Artist" TimeOfPost = "Time" Rating = {2}/>
+        <Post SongName = "Song name" UserName = "User name" Artist = "Artist" TimeOfPost = "Time" Rating = {4}/>
+        <Post SongName = "Song name" UserName = "User name" Artist = "Artist" TimeOfPost = "Time" Rating = {1}/>
       </div>
     </div>
   );
